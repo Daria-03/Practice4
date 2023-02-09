b826babf5049f869a500486b58b51bc95b5cc72f
Daria-03 <kvzss.rz.07.pa@mail.ru>
2023-02-09 16:24:45 +0600

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите степень: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("a^b=" + Math.Pow(a, b));
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+for (int i = 0; i < b; i++)
+    c *= a;
+{
+    Console.WriteLine("{0}^{1}={2}", a, b, c);
+    return 0;
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+void NumArray(int[] number)
+{
+    int length = number.Length;
+    int index = 0;
+    while (index < length)
+    {
+        number[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] numb)
+{
+    int count = numb.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(numb[position]);
+        position++;
+    }
+}
+
+int[] array = new int[8];
+NumArray(array);
+PrintArray(array);
